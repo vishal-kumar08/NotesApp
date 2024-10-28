7ed41f98893ad591cefd3a8e9cc2e54fe0e14114
vishal-Kumar08 <vishaldugri35@gmail.com>
2024-10-28 20:53:18 +0530

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by aman kumar on 28/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotesAppTests/NotesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppTests.swift
+//  NotesAppTests
+//
+//  Created by aman kumar on 28/10/24.
+//
+
+import Testing
+@testable import NotesApp
+
+struct NotesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
